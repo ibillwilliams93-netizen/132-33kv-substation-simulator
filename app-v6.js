19fdc3a10d974fe539a07fa285fe1ccb9742690b
ibillwilliams93-netizen <ibillwilliams93@gmail.com>
2026-09-25 13:07:36 +1200

    Reduce mouse wheel zoom sensitivity to 9.0

--- app-v6.js
+++ app-v6.js
@@ -16,7 +16,7 @@ controls.enableDamping=true;
 controls.dampingFactor=.055;
 controls.panSpeed=2.35;
 controls.rotateSpeed=1.55;
-controls.zoomSpeed=12.0;
+controls.zoomSpeed=9.0;
 controls.zoomToCursor=true;
 controls.minDistance=0.01;
 // Desktop navigation should react immediately but coast only slightly.
